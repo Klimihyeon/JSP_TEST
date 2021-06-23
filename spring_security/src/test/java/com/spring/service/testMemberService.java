ff1f1cc1995c6771ea490590b77bb651578af87e
Klimihyeon <lordnayo@gmail.com>
2021-06-23 15:59:44 +0900

    security

---
+++ spring_security/src/test/java/com/spring/service/testMemberService.java
@@ -0,0 +1,29 @@
+package com.spring.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.dto.MemberVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:com/spring/context/root-context.xml")
+@Transactional
+public class testMemberService {
+	
+	@Autowired
+	private MemberService service;
+	
+	@Test
+	public void testGetMember() throws Exception{
+		String id ="mimi";
+		
+		MemberVO member = service.getMember(id);
+		
+		Assert.assertEquals(id, member.getId());
+	}
+}
